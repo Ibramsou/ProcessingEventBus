08a6ee1ee31e403fa8b6b7551543a9da2653b753
Ibramsou <kasobanboui@gmail.com>
2023-06-21 14:24:20 +0200

    Add comments over event priority class

--- src/main/java/fr/bramsou/processing/eventbus/EventPriority.java
+++ src/main/java/fr/bramsou/processing/eventbus/EventPriority.java
@@ -1,5 +1,10 @@
 package fr.bramsou.processing.eventbus;
 
+/**
+ * event priority
+ * LOWEST = method will be called in last
+ * HIGH = method is called in first
+ */
 public enum EventPriority {
     HIGHEST,
     HIGH,
